439fa567ba652d4d7ecf85c25de5437efd94acdd
anisharma07 <anis42390@gmail.com>
2025-05-04 04:01:38 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
   },
-  esbuild: {
-    target: "es2021",
+  build: {
+    target: ["esnext"],
   },
 });
