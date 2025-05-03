11ec03be51d44caa10739779f154a44962ba1f2b
anisharma07 <anis42390@gmail.com>
2025-05-04 04:12:04 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,24 @@
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import fs from "fs";
 
+function bigIntLiteralPlugin() {
+  return {
+    name: "bigint-literal-rewrite",
+    setup(build) {
+      const bigintRE = /([^0-9A-Za-z$_.])(\d+)n\b/g;
+      build.onLoad({ filter: /\.[jt]s$/ }, async (args) => {
+        const text = await fs.promises.readFile(args.path, "utf8");
+        const transformed = text.replace(
+          bigintRE,
+          (_, prefix, num) => `${prefix}BigInt("${num}")`
+        );
+        return { contents: transformed, loader: "default" };
+      });
+    },
+  };
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
@@ -11,5 +28,11 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+  },
+  build: {
+    target: ["es2021"], // you can keep this
+    rollupOptions: {
+      plugins: [bigIntLiteralPlugin()],
+    },
   },
 });
